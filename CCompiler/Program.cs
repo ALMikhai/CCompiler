a1d82728e28b5f2e019106ad368cc44283fc0300
AlexanderMikhailenko <molotok539@gmail.com>
2021-03-26 19:39:25 +1000

    Add solution files

---
+++ CCompiler/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.IO;
+using CCompiler.Tokenizer;
+
+namespace CCompiler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
