f9406d709f3ed3ab68a1e8006fb3e17d9158566c
AlexanderMikhailenko <molotok539@gmail.com>
2021-03-26 19:41:20 +1000

    Add TokenizerException

---
+++ CCompiler/Tokenizer/TokenizerException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CCompiler.Tokenizer
+{
+    public class TokenizerException : Exception
+    {
+        private Position _position;
+        private string _message;
+
+        public TokenizerException(Position position, string message)
+        {
+            _position = position;
+            _message = message;
+        }
+
+        public override string ToString()
+        {
+            return $"{_position}: error: {_message}";
+        }
+    }
+}
