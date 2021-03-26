cdd99707aa2749a8f9244ebf667b5f4cc7dce9e0
AlexanderMikhailenko <molotok539@gmail.com>
2021-03-26 19:45:05 +1000

    Update Program (using tokenizer)

--- CCompiler/Program.cs
+++ CCompiler/Program.cs
@@ -8,6 +8,25 @@ namespace CCompiler
     {
         static void Main(string[] args)
         {
+            try
+            {
+                var tokenizer =
+                    new Tokenizer.Tokenizer(@"D:\Documents\All my projects\CCompiler\CCompiler\TestProgram.txt");
+                Token token;
+                do
+                {
+                    token = tokenizer.Get();
+                    Console.WriteLine(token);
+                } while (token.Type != TokenType.EOF);
+            }
+            catch (TokenizerException e)
+            {
+                Console.WriteLine(e);
+            }
+            catch (FileNotFoundException e)
+            {
+                Console.WriteLine(e);
+            }
         }
     }
 }
