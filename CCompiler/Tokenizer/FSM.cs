1bbe12f4c4dbffdb3bbf841fd2966e948b4ac24a
AlexanderMikhailenko <molotok539@gmail.com>
2021-03-26 19:40:16 +1000

    Add FSM class

---
+++ CCompiler/Tokenizer/FSM.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CCompiler.Tokenizer
+{
+    public enum FSMState
+    {
+        NONE,
+        END,
+        RUNNING,
+        ERROR
+    }
+
+    public abstract class FSM
+    {
+        public abstract FSMState GetState();
+        public abstract void ReadChar(Char ch);
+        public abstract void Reset(Position tokenPosition);
+        public abstract Token GetToken();
+        public abstract TokenizerException GetException();
+    }
+}
