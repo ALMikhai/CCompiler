31309c584d15b8a8174ff314bea615ad25ea2e6c
AlexanderMikhailenko <molotok539@gmail.com>
2021-05-28 01:19:15 +1000

    Add Jump statement parse and nodes

---
+++ CCompiler/Parser/StatementNodes.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using CCompiler.Tokenizer;
+
+namespace CCompiler.Parser
+{
+    public class JumpStat : Node
+    {
+        public KeywordType JumpType { get; }
+
+        public JumpStat(KeywordType type)
+        {
+            JumpType = type;
+        }
+        
+        public override NodeType Type => NodeType.JUMP;
+
+        public override string ToString(string indent, bool last)
+        {
+            return indent + NodePrefix(last) + $"{JumpType}" + "\r\n";
+        }
+    }
+
+    public class ReturnStat : Node
+    {
+        public Node Exp { get; }
+
+        public ReturnStat(Node exp)
+        {
+            Exp = exp;
+        }
+        
+        public override NodeType Type => NodeType.RETURN;
+
+        public override string ToString(string indent, bool last)
+        {
+            return indent + NodePrefix(last) + $"RETURN" + "\r\n" +
+                   Exp.ToString(indent + ChildrenPrefix(last), true);
+        }
+    }
+}
---
+++ CCompiler/Parser/StatementParsers.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using CCompiler.Tokenizer;
+
+namespace CCompiler.Parser
+{
+    public partial class SyntaxParser
+    {
+        /*
+         *jump_stat	: 'goto' id ';'
+            | 'continue' ';'
+            | 'break' ';'
+            | 'return' exp ';'
+            | 'return'	';'
+            ;
+         */
+
+        public IParseResult ParseJumpStat()
+        {
+            if (AcceptKeyword(KeywordType.CONTINUE))
+            {
+                if (ExceptOp(OperatorType.SEMICOLON))
+                {
+                    return new SuccessParseResult(new JumpStat(KeywordType.CONTINUE));
+                }
+            }
+
+            if (AcceptKeyword(KeywordType.BREAK))
+            {
+                if (ExceptOp(OperatorType.SEMICOLON))
+                {
+                    return new SuccessParseResult(new JumpStat(KeywordType.BREAK));
+                }
+            }
+            
+            if (AcceptKeyword(KeywordType.RETURN))
+            {
+                if (AcceptOp(OperatorType.SEMICOLON))
+                {
+                    return new SuccessParseResult(new JumpStat(KeywordType.RETURN));
+                }
+
+                var exp = ParseExp();
+                if (!exp.IsSuccess)
+                    return exp;
+                
+                if (ExceptOp(OperatorType.SEMICOLON))
+                {
+                    return new SuccessParseResult(new ReturnStat(exp.ResultNode));
+                }
+            }
+            
+            return new FailedParseResult("expected jump statement", _currentToken);
+        }
+    }
+}
